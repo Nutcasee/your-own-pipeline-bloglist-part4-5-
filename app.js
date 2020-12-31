edf7a2b2c2a263e34e6c24fae5066b3dad3a5e58
nutcasee <nexustivi@gmail.com>
2020-12-31 17:13:54 +0700

    to restart..

---
+++ app.js
@@ -0,0 +1,45 @@
+/* eslint-disable global-require */
+const cors = require('cors')
+const express = require('express')
+require('express-async-errors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
+const app = express()
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.set('useCreateIndex', true)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+mongoose.set('useFindAndModify', false)
+
+app.use(cors())
+// app.use(express.static('build'))
+app.use(express.json()) // hahaahhh super important for newbie, fuck 5h pass 2020sep25.0032fri
+app.use(middleware.tokenExtractor)
+// woa...woa... come first/after blogsRouter is important, hah, 2020oct08.0333pm thu
+
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
+app.use(middleware.errorHandler)
+
+module.exports = app
