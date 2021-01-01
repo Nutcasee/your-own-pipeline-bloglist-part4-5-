ddf318cdd10e60c5eee66aa7b6f1f5a13cccd71e
nutcasee <nexustivi@gmail.com>
2021-01-01 11:25:16 +0700

    add front, don't know what I do now dkwid

---
+++ build/precache-manifest.f8dc382518c3a29821ae66fac324ef7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c9735d646a94928717358e06a92060",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49a0d7a07ebe8e17a363",
+    "url": "/static/css/main.8e52a50f.chunk.css"
+  },
+  {
+    "revision": "d030ab4f7a64fa55c5b3",
+    "url": "/static/js/2.d318f655.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d318f655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a0d7a07ebe8e17a363",
+    "url": "/static/js/main.a9ba3603.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/index.js'], // "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
