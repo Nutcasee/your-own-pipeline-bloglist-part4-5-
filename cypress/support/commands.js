f77872a175a95e019f1c101512b9af568fb034bc
nutcasee <nexustivi@gmail.com>
2021-01-02 12:03:26 +0700

    fix some cypress command, 3000 -> 3001

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -30,7 +30,7 @@ Cypress.Commands.add('login', ({ username, password }) => {
     username, password
   }).then(({ body }) => {
     localStorage.setItem('loggedBlogappUser', JSON.stringify(body))
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3001')
   })
 })
 
@@ -44,5 +44,5 @@ Cypress.Commands.add('createBlog', ({ title, author, url, likes }) => {
     }
   })
 
-  cy.visit('http://localhost:3000')
+  cy.visit('http://localhost:3001')
 })
